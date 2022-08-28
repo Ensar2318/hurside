02baa7679aff571576a896018f6c8d2ca4940ba6
Ensar2318 <ensar163029@gmail.com>
2022-08-28 14:46:31 +0300

    asdasd

---
+++ js/my.js
@@ -0,0 +1,38 @@
+var swiper = new Swiper(".galerySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+      },
+});
+
+const lightbox = GLightbox({
+    touchNavigation: true,
+    loop: true,
+    autoplayVideos: true
+});
